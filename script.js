83d27b523ffe1ebf2e993371e4a8560d6e65205d
Hannah Smith <hannahpsmith1@gmail.com>
2020-06-22 20:48:34 -0700

    moving files from my-class-repo-kittens to here

---
+++ script.js
@@ -0,0 +1,100 @@
+document.getElementById("start-btn").addEventListener ("click", startQuiz)
+
+
+
+function startQuiz (){
+    document.getElementById("headerText").classList.add("hide")
+    document.getElementById("question-container").classList.remove("hide")
+    document.getElementById("start-btn").classList.add("hide")
+    populateQuestion(questions[0])
+    
+}
+
+function populateQuestion (questionObject){
+    var AnswerEl =document.getElementById("Answers")
+    var QuestionEl = document.getElementById("Question")
+    QuestionEl.innerHTML = questionObject.question
+    console.log(questionObject.answers)
+    for (i=0; i < questionObject.answers.length; i++){
+        var button = document.createElement("button")
+        button.innerHTML = "test"
+        console.log(questionObject.answers[i])
+
+        AnswerEl.appendChild(button)
+    }
+}
+
+
+function startTimer (){
+
+}
+
+function checkQuestion (){
+
+}
+
+function wrongAnswer () {
+
+}
+
+function endQuiz () {
+
+}
+
+function addQuestionToQuiz () {
+
+}
+
+function correctAnswer () {
+
+}
+
+
+
+const questions = [
+    {
+      question: 'Who was the 44th President of the USA',
+      answers: [
+        { text: 'Obama', correct: true },
+        { text: 'Bush', correct: false },
+        { text: 'Clinton', correct: false },
+        { text: 'Gore', correct: false }
+      ]
+    },
+    {
+      question: 'In what state is Denali National Park?',
+      answers: [
+        { text: 'California', correct: false },
+        { text: 'Washington', correct: false },
+        { text: 'New Mexico', correct: false },
+        { text: 'Alaska', correct: true }
+      ]
+    },
+    {
+      question: 'What company did Steve Jobs found? ',
+      answers: [
+        { text: 'Apple', correct: true },
+        { text: 'Microsoft', correct: false },
+        { text: 'Tesla', correct: false },
+        { text: 'Amazon', correct: false }
+      ]
+    },
+    {
+      question: 'Where is 30 Rock Located?',
+      answers: [
+        { text: 'Cleveland', correct: false },
+        { text: 'Los Angeles', correct: false },
+        { text: 'Boston', correct: false },
+        { text: 'New York City', correct: true }
+      ]
+    },
+    {
+      question: 'Who is Kim Kardashian Married to?',
+      answers: [
+        { text: 'Chris Humpries', correct: false },
+        { text: 'Dwayne Wade', correct: false },
+        { text: 'Kobe Bryant', correct: false },
+        { text: 'Kanye West', correct: true }
+      ]
+    }
+]
