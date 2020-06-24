75dcf07e3b8df841d86443fc5e975e1580da022f
Hannah Smith <hannahpsmith1@gmail.com>
2020-06-23 17:59:53 -0700

    fixed text population field

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("start-btn").addEventListener ("click", startQuiz)
 
 function startQuiz (){
     document.getElementById("headerText").classList.add("hide")
+    document.getElementById("supportText").classList.add("hide")
     document.getElementById("question-container").classList.remove("hide")
     document.getElementById("start-btn").classList.add("hide")
     populateQuestion(questions[0])
@@ -15,14 +16,73 @@ function populateQuestion (questionObject){
     var QuestionEl = document.getElementById("Question")
     QuestionEl.innerHTML = questionObject.question
     console.log(questionObject.answers)
-    for (i=0; i < questionObject.answers.length; i++){
-        var button = document.createElement("button")
-        button.innerHTML = "test"
-        console.log(questionObject.answers[i])
+    for (var i = 0; i < questions.question.answers.length; i++) {
+//       var button = $("<button>");
+//       button.addClass("button");
+//       button.attr("data-option", answers[i]);
+//       button.text(answers[i]);
+//       $("buttons").append(button);
+//     })
+    
+//     for (i=0; i < answers.length; i++){
+//         var button = document.createElement("button");
+//         // button.setAttribute(AnswersEl);
+//         // button.setAttribute ('class', 'btn');
+//         button.innerHTML = answers.text[i];
+//         console.log(questionObject.answers[i])
 
-        AnswerEl.appendChild(button)
-    }
-}
+//         AnswerEl.appendChild(button);
+//     }
+// }
+
+// for (var i = 0; i < questions.question.answers.length; i++) {
+//   var button = $("<button>");
+//   button.addClass("button");
+//   button.attr("data-option", answers[i]);
+//   button.text(answers[i]);
+//   $("buttons").append(button);
+// })
+
+//   // Inside the loop...
+
+//   // 2. Create a variable named "letterBtn" equal to $("<button>");
+//   var letterBtn = $("<button>");
+
+//   // 3. Then give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
+//   letterBtn.addClass("letter-button letter letter-button-color");
+
+//   // 4. Then give each "letterBtn" a data-attribute called "data-letter".
+//   letterBtn.attr("data-letter", letters[i]);
+
+//   // 5. Then give each "letterBtns" a text equal to "letters[i]".
+//   letterBtn.text(letters[i]);
+
+//   // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
+//   $("#buttons").append(letterBtn);
+
+// }
+// }
+// generateButtons()
+// Be sure to test that your code works for this major task, before proceeding to the next one!
+
+
+
+
+
+
+// function showQuestion(question) {
+//   questionElement.innerText = question.question
+//   question.answers.forEach(answer => {
+//     const button = document.createElement('button')
+//     button.innerText = answer.text
+//     button.classList.add('btn')
+//     if (answer.correct) {
+//       button.dataset.correct = answer.correct
+//     }
+//     button.addEventListener('click', selectAnswer)
+//     answerButtonsElement.appendChild(button)
+//   })
+// }
 
 
 function startTimer (){
